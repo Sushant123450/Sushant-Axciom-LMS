6c24b807f5baaea584cde20801269743a7039e33
Sushant123450 <Sushant123450.sk@gmail.com>
2024-12-31 21:19:53 +0530

    Models and  Controller Added

---
+++ server/models/item_model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+	{
+		itemType: {
+			type: String,
+			enum: ["Book", "Movie"],
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		dateOfProcurement: {
+			type: Date,
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+			min: 1,
+		},
+		bid: {
+			type: String,
+			required: true,
+		},
+		availability: {
+			type: Boolean,
+			required: true,
+		},
+
+		category: {
+			type: String,
+			enum: [
+				"science",
+				"economics",
+				"fiction",
+				"children",
+				"personal development",
+			],
+			required: true,
+		},
+		authorName: {
+			type: String,
+			required: true,
+		},
+		cost: {
+			type: Number,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Item", itemSchema);
